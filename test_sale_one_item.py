0a1e6db720a1ab65fe86b566cdf6023a2ed6ba47
Matt Clarke <matt.clarke@esss.se>
2021-02-05 11:44:58 +0100

    Refactoring...

--- test_sale_one_item.py
+++ test_sale_one_item.py
@@ -1,27 +1,8 @@
-from typing import Dict
-
-
-class UnknownBarcode(Exception):
-    pass
-
-
-class System:
-    def __init__(self):
-        self._items: Dict[str, int] = {"1234567890": 1234, "42424242": 42}
-        self._total = 0
-
-    def add_item_by_barcode(self, barcode: str):
-        try:
-            self._total = self._items[barcode]
-        except KeyError:
-            raise UnknownBarcode
-
-    def calculate_total(self):
-        return self._total
+from system import System, UnknownBarcode
 
 
 def test_can_add_one_item_and_retrieve_its_price_as_the_total():
-    system = System()
+    system = System({"1234567890": 1234, "42424242": 42})
 
     system.add_item_by_barcode("1234567890")
 
@@ -29,7 +10,7 @@ def test_can_add_one_item_and_retrieve_its_price_as_the_total():
 
 
 def test_can_add_a_different_item_and_retrieve_its_price_as_the_total():
-    system = System()
+    system = System({"1234567890": 1234, "42424242": 42})
 
     system.add_item_by_barcode("42424242")
 
@@ -37,7 +18,7 @@ def test_can_add_a_different_item_and_retrieve_its_price_as_the_total():
 
 
 def test_adding_unknown_barcode_raises_exception():
-    system = System()
+    system = System({"1234567890": 1234, "42424242": 42})
 
     try:
         system.add_item_by_barcode("unknown")

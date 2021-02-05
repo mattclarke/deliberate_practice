95e10e2d5ccbe0b467668367e19139b50c06de0c
Matthew D Jones <matthew.d.jones@tessella.com>
2021-02-05 10:40:01 +0000

    mob next [ci-skip]

--- test_sale_one_item.py
+++ test_sale_one_item.py
@@ -1,12 +1,23 @@
+from typing import Dict
+
+
+class UnknownBarcode(Exception):
+    pass
+
+
 class System:
     def __init__(self):
-        pass
+        self._items: Dict[str, int] = {"1234567890": 1234, "42424242": 42}
+        self._total = 0
 
-    def add_item_by_barcode(self, barcode):
-        pass
+    def add_item_by_barcode(self, barcode: str):
+        try:
+            self._total = self._items[barcode]
+        except KeyError:
+            raise UnknownBarcode
 
     def calculate_total(self):
-        return 1234
+        return self._total
 
 
 def test_can_add_one_item_and_retrieve_its_price_as_the_total():
@@ -15,3 +26,20 @@ def test_can_add_one_item_and_retrieve_its_price_as_the_total():
     system.add_item_by_barcode("1234567890")
 
     assert system.calculate_total() == 1234
+
+
+def test_can_add_a_different_item_and_retrieve_its_price_as_the_total():
+    system = System()
+
+    system.add_item_by_barcode("42424242")
+
+    assert system.calculate_total() == 42
+
+
+def test_adding_unknown_barcode_raises_exception():
+    system = System()
+
+    try:
+        system.add_item_by_barcode("unknown")
+    except UnknownBarcode:
+        pass
